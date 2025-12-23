e22f7281b348c7d72a1548c3ba711f7c7af1783b
sxmiru <samirulawrance9@gmail.com>
2025-12-23 23:56:15 +0530

    Search products 📝

--- src/pages/client/productPage.jsx
+++ src/pages/client/productPage.jsx
@@ -8,16 +8,26 @@ export default function ProductPage() {
 
 const [products, setProducts] = useState([]);
 const [isLoading, setIsLoading] = useState(true);
+const [query, setQuery] = useState("");
 
 useEffect(
     ()=> {
         if(isLoading){
-            axios.get(import.meta.env.VITE_BACKEND_URL + "/api/products").then(
+            if(query == ""){
+                axios.get(import.meta.env.VITE_BACKEND_URL + "/api/products").then(
                 (res)=> {
                     setProducts(res.data)
                     setIsLoading(false)
                 }
             )
+            }else{
+                axios.get(import.meta.env.VITE_BACKEND_URL + "/api/products/search/"+query).then(
+                (res)=> {
+                    setProducts(res.data)
+                    setIsLoading(false)
+                }
+            )
+            } 
         }
     },
     [isLoading]
@@ -25,6 +35,18 @@ useEffect(
 
     return(
         <div className="w-full h-full">
+            <div className="w-full h-[100px] flex justify-center items-center">
+                <input
+                    type="text"
+                    placeholder="Search products"
+                    value={query}
+                    onChange={(e)=>{
+                        setQuery(e.target.value)
+                        setIsLoading(true)
+                    }}
+                    className="w-[400px] h-[40px] border border-gray-300 rounded-lg p-[10px] m-[10px] text-lg"
+                />
+            </div>
             {
                 isLoading ? <Loader/> : 
                 <div className="w-full flex flex-wrap gap-[30px] justify-center items-center p-[20px]">
