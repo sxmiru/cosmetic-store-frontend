3a1ebd831fd1b746cd1078b10bd85bc0ef7fb393
sxmiru <samirulawrance9@gmail.com>
2025-12-24 22:24:47 +0530

    Secure admin 🔐

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import { FaBoxArchive } from "react-icons/fa6";
 import { FaBagShopping } from "react-icons/fa6";
 import { IoPeople } from "react-icons/io5";
@@ -7,26 +7,64 @@ import ProductsAdminPage from "./admin/productsAdminPage";
 import AddProductPage from "./admin/addProductAdminPage";
 import UpdateProductPage from "./admin/updateProductAdmin";
 import OrdersPageAdmin from "./admin/ordersAdminPage.jsx";
+import Loader from "../components/loader.jsx";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function AdminPage() {
+
+const navigate = useNavigate();
+const [adminValidated, setAdminValidated] = useState(false);
+
+useEffect(
+  ()=>{
+    const token = localStorage.getItem("token");
+    if(token == null){
+      toast.error("You are not logged in")
+      navigate("/login");
+    }else{
+      axios.get(import.meta.env.VITE_BACKEND_URL+"/api/users/",{
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+      }).then(
+        (response)=>{
+          if(response.data.role == "admin"){
+            setAdminValidated(true);
+          }else{
+            toast.error("You are not authorized")
+            navigate("/login")
+          }
+        }
+      ).catch((error)=>{
+        toast.error("You are not authorized")
+        navigate("/login")
+      });
+    }
+  },[]
+);
+
   return (
     <div className="w-full h-screen flex">
-      <div className="w-[300px] h-full flex flex-col items-center">
-        <span className="text-3xl font-bold my-5">Admin Panel</span>
-        <Link className="flex items-center p-[20px] row h-[60px] w-full text-xl gap-[25px]" to="/admin/products"><FaBoxArchive/>Products</Link>
-        <Link className="flex items-center p-[20px] row h-[60px] w-full text-xl gap-[25px]" to="/admin/orders"><FaBagShopping/>Orders</Link>
-        <Link className="flex items-center p-[20px] row h-[60px] w-full text-xl gap-[25px]" to="/admin/users"><IoPeople/>Users</Link>
-        <Link className="flex items-center p-[20px] row h-[60px] w-full text-xl gap-[25px]" to="/admin/settings"><IoMdSettings/>Settings</Link>
-      </div>
-      <div className="w-[calc(100%-300px)] h-full">
-        <Routes path="/">
-          <Route path="/" element={<h1>Dashboard</h1>} />
-          <Route path="/products" element={<ProductsAdminPage/>} />
-          <Route path="/newProduct" element={<AddProductPage/>}/>
-          <Route path="/updateProduct" element={<UpdateProductPage/>}/>
-          <Route path="/orders" element={<OrdersPageAdmin/>} />
-        </Routes>
-      </div>
+      {adminValidated ? <>
+        <div className="w-[300px] h-full flex flex-col items-center">
+          <span className="text-3xl font-bold my-5">Admin Panel</span>
+          <Link className="flex items-center p-[20px] row h-[60px] w-full text-xl gap-[25px]" to="/admin/products"><FaBoxArchive/>Products</Link>
+          <Link className="flex items-center p-[20px] row h-[60px] w-full text-xl gap-[25px]" to="/admin/orders"><FaBagShopping/>Orders</Link>
+          <Link className="flex items-center p-[20px] row h-[60px] w-full text-xl gap-[25px]" to="/admin/users"><IoPeople/>Users</Link>
+          <Link className="flex items-center p-[20px] row h-[60px] w-full text-xl gap-[25px]" to="/admin/settings"><IoMdSettings/>Settings</Link>
+        </div>
+        <div className="w-[calc(100%-300px)] h-full">
+          <Routes path="/">
+            <Route path="/" element={<h1>Dashboard</h1>} />
+            <Route path="/products" element={<ProductsAdminPage/>} />
+            <Route path="/newProduct" element={<AddProductPage/>}/>
+            <Route path="/updateProduct" element={<UpdateProductPage/>}/>
+            <Route path="/orders" element={<OrdersPageAdmin/>} />
+          </Routes>
+        </div>
+      </> : <Loader/>}
     </div>
   );
 }
